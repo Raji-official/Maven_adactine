1a293c6790ee41ae264d6f7f2aa0e2b9d27477b2
raji <rajfernanto765@gmail.com>
2024-01-01 19:01:17 +0530

    first commit

--- src/test/java/com/amazon/Amazon.java
+++ src/test/java/com/amazon/Amazon.java
@@ -1,90 +1,115 @@
 package com.amazon;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.Select;
 
 public class Amazon {
 
-	public static void main(String[] args) throws InterruptedException {
-
+	public static void main(String[] args) throws InterruptedException, IOException {
 
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.ajio.com/shop/sale?gad_source=1&gclid=Cj0KCQiA4Y-sBhC6ARIsAGXF1g6ctxIbPRnLWL-o35m9UJtbJ8-voRitihQS-RHXz4ECINvoaLSi7C0aAl-SEALw_wcB");
+		driver.get("https://www.amazon.in/");
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		
-		Actions action=new Actions(driver);
 		
-		WebElement findElement = driver.findElement(By.xpath("//span[text()='HOME AND KITCHEN']"));
+		WebElement findElement = driver.findElement(By.xpath("//select[@id='searchDropdownBox']"));
+		String text2 = findElement.getText();
+		System.out.println(text2);
+		
+		Select location= new Select(findElement);
+		System.out.println(location);
+		location.selectByVisibleText("Books");
+		
+		String inp="comic";
+		driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys(inp);
 		
 		
-		
-		action.moveToElement(findElement).build().perform();
-		
-		WebElement bed = driver.findElement(By.xpath("//a[text()='Bedsheets']"));
+	//
+		Thread.sleep(3000);
+		WebElement comic = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+		String inptext = comic.getText();
+		System.out.println(inptext);
 
 		
 		
-		action.moveToElement(bed).build().perform();
-		Thread.sleep(1000);
-		bed.click();
+		 List<WebElement> findElements = driver.findElements(By.xpath("//input[@id='twotabsearchtextbox']//parent::div//parent::div//ancestor::header//following-sibling::div//div[@class='s-suggestion-container']"));
 		
-		
-		List<WebElement> list_0f_bedprice= driver.findElements(By.xpath("//span[@class='price  ']"));
+		for(WebElement data:findElements) {
+			String text = data.getText();
+			System.out.println(text);
+			if(text.equalsIgnoreCase(inp)) {
+			System.out.println("succes");
+			System.out.println(text);
+			
+			
+			Actions actions=new Actions(driver);
+			
+			  WebElement findElement3 = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']//parent::div//parent::div//ancestor::header//following-sibling::div//div[@class='s-suggestion-container']//div[@aria-label='comic']"));
+			  
+			findElement3.click();
+			driver.findElement(By.xpath("(//span[text()='Results']//parent::div//following::script//following::div//span[starts-with(text(), 'Superman')])")).click();
+			
+			Set<String> windowHandles = driver.getWindowHandles();
+		      for (String handle : windowHandles) {
+		          driver.switchTo().window(handle);
+		      }
+	//
+			
+			driver.switchTo();
+			String title = driver.getTitle();
+			System.out.println(title);
+			
+			
+			String substring = title.substring(0, 56);
+			System.out.println(substring);
+			
+			WebElement findElement2 = driver.findElement(By.xpath("//span[@id='productTitle']"));
+			String book_name = findElement2.getText();
+			System.out.println(book_name);
+			
+			if(substring.equals(book_name)) {
+			
+				TakesScreenshot screen=(TakesScreenshot)driver;
+			    File decFile=screen.getScreenshotAs(OutputType.FILE);
+			    File des=new File("C:\\Users\\Welcome\\eclipse-workspace\\Selenum_demo\\screenshot\\amazon_2.png");
+			    FileHandler.copy(decFile, des);
+			    
+				 driver.findElement(By.xpath("//input[@id='buy-now-button']")).click();
 
-		List<WebElement> list_0f_bedname = driver.findElements(By.xpath("//div[@class='nameCls']"));
-		
-		String bedname;
-		String bedprice;
-		int integer_product_price;
-		
-		HashMap<Integer, String> data_map = new HashMap<Integer, String>();
-		
-		for (int i = 0; i < list_0f_bedname.size(); i++) {
-			bedname=list_0f_bedname.get(i).getText();
-			bedprice=list_0f_bedprice.get(i).getText();
-		    bedprice= bedprice.replace("₹", "").trim();
-		    bedprice=bedprice.replace(",","").trim();
-		    System.out.println(bedprice);
-		    integer_product_price=Integer.parseInt(bedprice);
-		   
-		    data_map.put( integer_product_price, bedname);
+			}else {
+				System.out.println("error");
+			}
+			Thread.sleep(1000);
+			TakesScreenshot screen=(TakesScreenshot)driver;
+		    File decFile=screen.getScreenshotAs(OutputType.FILE);
+		    File des=new File(".\\screenshot\\amazon_1.png");
+		    FileHandler.copy(decFile, des);
+			
+		    Thread.sleep(1000);
+			driver.close();
+			Thread.sleep(1000);
+			driver.quit();
+			
+			
+			}
 			
 		}
-		
-
-		Set<Integer> keySet = data_map.keySet();
-		ArrayList<Integer> product_price_list =new  ArrayList<Integer>(keySet);
-		System.out.println("product_price_list   :" +product_price_list);
-		
-		Collections.sort(product_price_list);
-		
-		Integer max_price = product_price_list.get(product_price_list.size()-1);
-		Integer min_price = product_price_list.get(0);
-		
-		   System.out.println("High Product Price is: " + max_price + " Product name is: " + data_map.get(max_price));
-			System.out.println("Low Product Price is: " + min_price + " Product name is: " + data_map.get(min_price));
-		
-
-	    
-
-	 
-		    	
-		    
-
-		
-
+			
 		
 		
 	}
